cfb64b0b933af47c90927ec9968f6648fee84714
Pim Merks <pimmerks@users.noreply.github.com>
2022-06-04 12:45:13 +0200

    Re-added `junit` to karma reporters

--- projects/auth0-angular/karma.conf.js
+++ projects/auth0-angular/karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
         { type: 'text-summary' },
       ],
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
