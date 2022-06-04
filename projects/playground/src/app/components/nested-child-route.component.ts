37938317a251586fdb2597ebde79863d4c3ee810
Pim Merks <pimmerks@users.noreply.github.com>
2022-06-04 16:38:13 +0200

    Migrated to ESLint

--- projects/playground/src/app/components/nested-child-route.component.ts
+++ projects/playground/src/app/components/nested-child-route.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nested-child-route',
@@ -9,8 +9,6 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: [],
 })
-export class NestedChildRouteComponent implements OnInit {
+export class NestedChildRouteComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
